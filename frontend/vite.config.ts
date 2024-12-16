82d93a75756aa61f5f606ea8b7668cf14776a4ef
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-16 21:34:42 +0100

    Fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/HubCook",
 	server: {
-		host: "0.0.0.0",
+		host: true,
+		watch: {
+			usePolling: true,
+		},
 	},
 });
