facc01bd2cb0703342fdad0de900438eab74dd3b
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-17 15:55:15 +0100

    Add link for page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,30 +1,32 @@
+import { NavLink } from "react-router";
+
 export default function Header() {
 	return (
 		<header className="flex justify-between py-3 px-8 shadow-md items-center">
-			<div className="">
+			<NavLink to="/">
 				<i className="fa-solid fa-kitchen-set text-3xl mr-2" />
 				<span className="text-2xl font-bold">HubCook</span>
-			</div>
+			</NavLink>
 			<ul className="flex gap-x-3">
 				<li>
-					<a href="">Accueil</a>
+					<NavLink to="/">Accueil</NavLink>
 				</li>
 				<li>
-					<a href="">Recettes</a>
+					<NavLink to="/recipes">Recettes</NavLink>
 				</li>
 			</ul>
 			<ul className="flex gap-x-3">
 				<li>
-					<a href="">Connexion</a>
+					<NavLink to="/">Connexion</NavLink>
 				</li>
 				<li>
-					<a href="">Inscription</a>
+					<NavLink to="/register">Inscription</NavLink>
 				</li>
 				<li>
-					<a href="">Profil</a>
+					<NavLink to="/">Profil</NavLink>
 				</li>
 				<li>
-					<a href="">Deconnexion</a>
+					<NavLink to="/">Deconnexion</NavLink>
 				</li>
 			</ul>
 		</header>
